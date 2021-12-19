17994ec92305c75800decb26ff0c41d70c944723
3기-1반 애범 <gogot1103@naver.com>
2021-12-19 19:41:57 +0900

    vanillaJS로 TODO리스트 기능  셋업

--- src/index.js
+++ src/index.js
@@ -1,47 +1,31 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("p")
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-//변수를 선언함으로써 더 안정성 있게!
-const ADD = "ADD"
-const MINUS = "MINUS"
-
-const countModifier = (count=0, action) => {
-
-    switch(action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count -1;
+const reducer = (state = [], action) => {
+    switch(action.type){
+        case ADD_TODO:
+            return[];
+        case DELETE_TODO:{
+            return[];
+        }
         default:
-            return count;
-
+            return state
     }
-};
-
-const countStore = createStore(countModifier);
-
-
-
-const onChange = () => {
-    number.innerText = countStore.getState();
-};
-
-// subscribe: store 안에 있는 변화 감지, store.subscribe(함수)// store 변화 시에 함수 자동 실행
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-    countStore.dispatch({type:ADD})
 }
 
-const handleMinus = () => {
-    countStore.dispatch({type:MINUS})
+const store = createStore(reducer)
+
+const onSubmit = (e) => {
+    e.preventDefalut();
+    const toDo = input.value;
+    store.dispatch({type:ADD_TODO, text:toDo})
 }
 
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+
+form.addEventListener("submit", onSubmit)
