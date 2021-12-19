e42f60bb1b364ee6d7b72c7e6b5ce7c457cbba3a
3기-1반 애범 <gogot1103@naver.com>
2021-12-19 18:01:38 +0900

    vanilla-js로 count-redux 연습

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("p")
 
-let count = 0
-
-number.innerText = count;
-const updateText = () => {
-    number.innerText = count;
+number.innerText = 0;
+const countModifier = (count=0, action) => {
+    
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type ==="MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
+
+const countStore = createStore(countModifier);
+
+
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+// subscribe: store 안에 있는 변화 감지, store.subscribe(함수)// store 변화 시에 함수 자동 실행
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-    count = count + 1;
-    updateText();
-};
+    countStore.dispatch({type:"ADD"})
+}
 
 const handleMinus = () => {
-    count = count - 1;
-    updateText();
+    countStore.dispatch({type:"MINUS"})
+}
 
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
